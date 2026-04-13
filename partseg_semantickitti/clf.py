6c991f68a799bbe63430ae3cb766abe12a1745df
simerlc <149055420+simerlc@users.noreply.github.com>
2026-04-13 16:16:32 +0800

    Add files via upload

---
+++ partseg_semantickitti/clf.py
@@ -0,0 +1,215 @@
+import open3d
+import argparse
+import os
+import time
+import h5py
+import datetime
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+from tqdm import tqdm
+import my_log as log
+
+from data_utils.ModelNetDataLoader import ModelNetDataLoader, load_data, class_names
+from utils import test_clf, save_checkpoint, select_avaliable, mkdir
+from model.pointnet2 import PointNet2ClsMsg
+from model.pointnet import PointNetCls, feature_transform_reguliarzer
+
+def parse_args(notebook = False):
+    parser = argparse.ArgumentParser('PointNet')
+    parser.add_argument('--model_name', default='pointnet', help='pointnet or pointnet2')
+    parser.add_argument('--mode', default='train', help='train or eval')
+    parser.add_argument('--batch_size', type=int, default=16, help='batch size in training')
+    parser.add_argument('--epoch', default=100, type=int, help='number of epoch in training')
+    parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
+    parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
+    parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
+    parser.add_argument('--pretrain', type=str, default=None, help='whether use pretrain model')
+    parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate of learning rate')
+    parser.add_argument('--feature_transform', default=False, help="use feature transform in pointnet")
+    parser.add_argument('--augment', default=False, action='store_true', help="Enable data augmentation")
+    if notebook:
+        return parser.parse_args([])
+    else:
+        return parser.parse_args()
+
+def train(args):
+    experiment_dir = mkdir('./experiment/')
+    checkpoints_dir = mkdir('./experiment/clf/%s/'%(args.model_name))
+    train_data, train_label, test_data, test_label = load_data('experiment/data/modelnet40_ply_hdf5_2048/')
+
+    trainDataset = ModelNetDataLoader(train_data, train_label, data_augmentation = args.augment)
+    trainDataLoader = DataLoader(trainDataset, batch_size=args.batch_size, shuffle=True)
+
+    testDataset = ModelNetDataLoader(test_data, test_label)
+    testDataLoader = torch.utils.data.DataLoader(testDataset, batch_size=args.batch_size, shuffle=False)
+
+    log.info('Building Model',args.model_name)
+    if args.model_name == 'pointnet':
+        num_class = 40
+        model = PointNetCls(num_class,args.feature_transform).cuda()  
+    else:
+        model = PointNet2ClsMsg().cuda()
+
+    torch.backends.cudnn.benchmark = True
+    model = torch.nn.DataParallel(model).cuda()
+    log.debug('Using gpu:',args.gpu)
+
+    if args.pretrain is not None:
+        log.info('Use pretrain model...')
+        state_dict = torch.load(args.pretrain)
+        model.load_state_dict(state_dict)
+        init_epoch = int(args.pretrain[:-4].split('-')[-1])
+        log.info('start epoch from', init_epoch)
+    else:
+        log.info('Training from scratch')
+        init_epoch = 0
+
+    if args.optimizer == 'SGD':
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    elif args.optimizer == 'Adam':
+        optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=args.learning_rate,
+            betas=(0.9, 0.999),
+            eps=1e-08,
+            weight_decay=args.decay_rate
+        )
+
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+    LEARNING_RATE_CLIP = 1e-5
+
+    global_epoch = 0
+    global_step = 0
+    best_tst_accuracy = 0.0
+
+    log.info('Start training...')
+    for epoch in range(init_epoch,args.epoch):
+        scheduler.step()
+        lr = max(optimizer.param_groups[0]['lr'],LEARNING_RATE_CLIP)
+
+        log.debug(job='clf',model=args.model_name,gpu=args.gpu,epoch='%d/%s' % (epoch, args.epoch),lr=lr)
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+        for batch_id, data in tqdm(enumerate(trainDataLoader, 0), total=len(trainDataLoader), smoothing=0.9):
+            points, target = data
+            target = target[:, 0]
+            points = points.transpose(2, 1)
+            points, target = points.cuda(), target.cuda()
+            optimizer.zero_grad()
+            model = model.train()
+            pred, trans_feat = model(points)
+            loss = F.nll_loss(pred, target.long())
+            if args.feature_transform and args.model_name == 'pointnet':
+                loss += feature_transform_reguliarzer(trans_feat) * 0.001
+            loss.backward()
+            optimizer.step()
+            global_step += 1
+        
+        log.debug('clear cuda cache')
+        torch.cuda.empty_cache()
+
+        acc = test_clf(model, testDataLoader)
+        log.info(loss='%.5f' % (loss.data))
+        log.info(Test_Accuracy='%.5f' % acc)
+
+        if acc >= best_tst_accuracy:
+            best_tst_accuracy = acc
+            fn_pth = 'clf-%s-%.5f-%04d.pth'%(args.model_name, acc, epoch)
+            log.debug('Saving model....', fn_pth)
+            torch.save(model.state_dict(), os.path.join(checkpoints_dir,fn_pth))
+        global_epoch += 1
+
+    log.info(Best_Accuracy = best_tst_accuracy)
+    log.info('End of training...')
+
+def evaluate(args):
+    test_data, test_label = load_data('experiment/data/modelnet40_ply_hdf5_2048/', train = False)
+    testDataset = ModelNetDataLoader(test_data, test_label)
+    testDataLoader = torch.utils.data.DataLoader(testDataset, batch_size=args.batch_size, shuffle=False)
+
+    log.debug('Building Model',args.model_name)
+    if args.model_name == 'pointnet':
+        num_class = 40
+        model = PointNetCls(num_class,args.feature_transform)
+    else:
+        model = PointNet2ClsMsg()
+
+    torch.backends.cudnn.benchmark = True
+    model = torch.nn.DataParallel(model).cuda()
+    log.debug('Using gpu:',args.gpu)
+
+    if args.pretrain is None:
+        log.err('No pretrain model')
+        return
+
+    log.debug('Loading pretrain model...')
+    state_dict = torch.load(args.pretrain)
+    model.load_state_dict(state_dict)
+
+    acc = test_clf(model.eval(), testDataLoader)
+    log.msg(Test_Accuracy='%.5f' % (acc))
+
+def vis(args):
+    test_data, test_label = load_data(root, train = False)
+    log.info(test_data=test_data.shape,test_label=test_label.shape)
+
+    log.debug('Building Model',args.model_name)
+    if args.model_name == 'pointnet':
+        num_class = 40
+        model = PointNetCls(num_class,args.feature_transform).cuda()  
+    else:
+        model = PointNet2ClsMsg().cuda()
+
+    torch.backends.cudnn.benchmark = True
+    model = torch.nn.DataParallel(model)
+    model.cuda()
+    log.info('Using multi GPU:',args.gpu)
+
+    if args.pretrain is None:
+        log.err('No pretrain model')
+        return
+
+    log.debug('Loading pretrain model...')
+    checkpoint = torch.load(args.pretrain)
+    model.load_state_dict(checkpoint)
+    model.eval()
+
+    log.info('Press space to exit, press Q for next frame')
+    
+    for idx in range(test_data.shape[0]):
+        point_np = test_data[idx:idx+1]
+        gt = test_label[idx][0]
+
+        points = torch.from_numpy(point_np)
+        points = points.transpose(2, 1).cuda()
+
+        pred, trans_feat = model(points)
+        pred_choice = pred.data.max(1)[1]
+        log.info(gt=class_names[gt], pred_choice=class_names[pred_choice.cpu().numpy().item()])
+
+        point_cloud = open3d.geometry.PointCloud()
+        point_cloud.points = open3d.utility.Vector3dVector(point_np[0])
+
+        vis = open3d.visualization.VisualizerWithKeyCallback()
+        vis.create_window()
+        vis.get_render_option().background_color = np.asarray([0, 0, 0])
+        vis.add_geometry(point_cloud)
+        vis.register_key_callback(32, lambda vis: exit())
+        vis.run()
+        vis.destroy_window()
+
+if __name__ == '__main__':
+    args = parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    if args.mode == "train":
+        train(args)
+    if args.mode == "eval":
+        evaluate(args)
+    if args.mode == "vis":
+        vis(args)
---
+++ partseg_semantickitti/pcdvis.py
@@ -0,0 +1,154 @@
+import open3d
+import argparse
+import os
+import time
+import json
+import h5py
+import datetime
+import cv2
+import yaml
+import colorsys
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+import my_log as log
+
+from model.pointnet import PointNetSeg, feature_transform_reguliarzer
+from model.pointnet2 import PointNet2SemSeg
+from model.utils import load_pointnet
+
+from pcdseg import parse_args
+from data_utils.SemKITTI_Loader import pcd_normalize
+from data_utils.kitti_utils import Semantic_KITTI_Utils
+
+KITTI_ROOT = os.environ['KITTI_ROOT']
+
+class Window_Manager():
+    def __init__(self):
+        self.param = open3d.io.read_pinhole_camera_parameters('config/ego_view.json')
+        self.vis = open3d.visualization.VisualizerWithKeyCallback()
+        self.vis.create_window(width=800, height=800, left=100)
+        self.vis.register_key_callback(32, lambda vis: exit())
+        self.vis.get_render_option().load_from_json('config/render_option.json')
+        self.pcd = open3d.geometry.PointCloud()
+    
+    def update(self, pts_3d, colors):
+        self.pcd.points = open3d.utility.Vector3dVector(pts_3d)
+        self.pcd.colors = open3d.utility.Vector3dVector(colors/255)
+        self.vis.remove_geometry(self.pcd)
+        self.vis.add_geometry(self.pcd)
+        self.vis.get_view_control().convert_from_pinhole_camera_parameters(self.param)
+        self.vis.update_geometry(self.pcd)
+        self.vis.poll_events()
+        self.vis.update_renderer()
+
+    def capture_screen(self,fn):
+        self.vis.capture_screen_image(fn, False)
+
+def export_video():
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    out = cv2.VideoWriter('experiment/pn_compare.avi',fourcc, 15.0, (int(1600*0.8),int(740*0.8)))
+
+    # mkdir('experiment/imgs/%s/'%(args.model_name))
+    # vis_handle.capture_screen('experiment/imgs/%s/%d_3d.png'%(args.model_name,i))
+    # cv2.imwrite('experiment/imgs/%s/%d_sem.png'%(args.model_name, i), img_semantic)
+
+    for index in range(100, 320):
+        pn_3d = cv2.imread('experiment/imgs/pointnet/%d_3d.png' % (index))
+        pn_sem = cv2.imread('experiment/imgs/pointnet/%d_sem.png' % (index))
+        pn2_3d = cv2.imread('experiment/imgs/pointnet2/%d_3d.png' % (index))
+        pn2_sem = cv2.imread('experiment/imgs/pointnet2/%d_sem.png' % (index))
+
+        pn_3d = pn_3d[160:650]
+        pn2_3d = pn2_3d[160:650]
+
+        pn_sem = cv2.resize(pn_sem, (800, 250))
+        pn2_sem = cv2.resize(pn2_sem, (800, 250))
+
+        pn = np.vstack((pn_3d, pn_sem))
+        pn2 = np.vstack((pn2_3d, pn2_sem))
+
+        cv2.putText(pn, 'PointNet', (20, 100), font,1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(pn, 'PointNet', (20, 520), font,1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(pn2, 'PointNet2', (20, 100), font,1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(pn2, 'PointNet2', (20, 520), font,1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        merge = np.hstack((pn, pn2))
+        class_names = ['unlabelled', 'vehicle', 'human', 'ground', 'structure', 'nature']
+        colors = [[255, 255, 255],[245, 150, 100],[30, 30, 255],[255, 0, 255],[0, 200, 255],[0, 175, 0]]
+        for i,(name,c) in enumerate(zip(class_names, colors)):
+            cv2.putText(merge, name, (200 + i * 200, 50), font,1, [c[2],c[1],c[0]], 2, cv2.LINE_AA)
+
+        cv2.line(merge,(0,70),(1600,70),(255,255,255),2)
+        cv2.line(merge,(800,70),(800,1300),(255,255,255),2)
+
+        merge = cv2.resize(merge,(0,0),fx=0.8,fy=0.8)
+        # cv2.imshow('merge', merge)
+        # if 32 == waitKey(1):
+        #     break
+        out.write(merge)
+
+        print(index)
+    out.release()
+
+def vis(args):
+    part = '01'
+    args.subset ='inview'
+    args.model_name = 'pointnet'
+
+    kitti_utils = Semantic_KITTI_Utils(KITTI_ROOT, subset=args.subset)
+
+    vis_handle = Window_Manager()
+    if args.model_name == 'pointnet':
+        args.pretrain = 'checkpoints/pointnet-inview-0.52077-0018.pth'
+    else:
+        args.pretrain = 'checkpoints/pointnet2-inview-0.55884-0001.pth'
+
+    model = load_pointnet(args.model_name, kitti_utils.num_classes, args.pretrain)
+
+    for index in range(0, kitti_utils.get_max_index(part)):
+        point_cloud, label = kitti_utils.get(part, index, load_image=True)
+        
+        # resample point cloud
+        length = point_cloud.shape[0]
+        npoints = 25000
+        choice = np.random.choice(length, npoints, replace=True)
+        point_cloud = point_cloud[choice]
+        label = label[choice]
+
+        pts_3d = point_cloud[:,:3]
+        pcd = pcd_normalize(point_cloud)
+
+        with log.Tick():
+            points = torch.from_numpy(pcd).unsqueeze(0).transpose(2, 1).cuda()
+            
+            with torch.no_grad():
+                if args.model_name == 'pointnet':
+                    logits, _ = model(points)
+                else:
+                    logits = model(points)
+                pred = logits[0].argmax(-1).cpu().numpy()
+
+            print(index, pred.shape, end='')
+
+        # pts_2d = kitti_utils.project_3d_to_2d(pts_3d)
+        pts_2d = kitti_utils.torch_project_3d_to_2d(pts_3d)
+
+        vis_handle.update(pts_3d, kitti_utils.colors[pred])
+        sem_img = kitti_utils.draw_2d_points(pts_2d, kitti_utils.colors_bgr[pred])
+
+        cv2.imshow('semantic', sem_img)
+        cv2.imshow('frame', cv2.cvtColor(kitti_utils.frame,cv2.COLOR_BGR2RGB))
+        if 32 == cv2.waitKey(1):
+            break
+
+if __name__ == '__main__':
+    args = parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    vis(args)
